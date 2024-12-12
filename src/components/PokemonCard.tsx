0eff600a79ce3f5f9feed0ecea39ef2795547374
Luigi-44 <luigi.rondeau@gmail.com>
2024-12-12 12:31:50 +0100

    ajout de la ternaire

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -4,9 +4,7 @@ const PokemonCard: React.FC = () => {
 	// Carte global du Pokemon
 	const pokemonList = [
 		{
-			name: "bulbasaur",
-			imgSrc:
-				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+			name: "inconnu",
 		},
 		{
 			name: "mew",
@@ -20,6 +18,11 @@ const PokemonCard: React.FC = () => {
 
 	return (
 		<figure>
+			{pokemon.imgSrc != null ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
 			<img src={pokemon.imgSrc} alt={pokemon.name} />
 			<figcaption>{pokemon.name}</figcaption>
 		</figure>
