b802b7c015c0700b04277f08c71f924cd6e65553
Luigi-44 <luigi.rondeau@gmail.com>
2024-12-12 12:23:29 +0100

    ajout de la ternaire et ma prop est fonctionelle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import PokemonCard from "./components/PokemonCard";
+import "./App.css";
 
-const App: React.FC = () => {
+function App() {
+	const pokemonList = [
+		{
+			name: "bulbasaur",
+			imgSrc:
+				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+		},
+		{
+			name: "mew",
+			imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+		},
+		{
+			name: "Pokemon inconnu au bataillon",
+			
+		},
+	];
 	return (
 		<div>
-			<PokemonCard />
+			<PokemonCard pokemon={pokemonList[2]} />
 		</div>
 	);
-};
+}
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,29 +1,16 @@
 import "./PokemonCard.css";
 
-const PokemonCard: React.FC = () => {
-	// Carte global du Pokemon
-	const pokemonList = [
-		{
-			name: "bulbasaur",
-			imgSrc:
-				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-		},
-		{
-			name: "mew",
-			imgSrc:
-				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
-		},
-	];
-
-	// Choix du Pokemon
-	const pokemon = pokemonList[0];
-
+function PokemonCard({ pokemon }) {
 	return (
 		<figure>
-			<img src={pokemon.imgSrc} alt={pokemon.name} />
+			{pokemon.imgSrc != null ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>Pokemon non debloqué</p>
+			)}
 			<figcaption>{pokemon.name}</figcaption>
 		</figure>
 	);
-};
+}
 
 export default PokemonCard;
